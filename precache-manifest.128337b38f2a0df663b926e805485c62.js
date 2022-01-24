139e0906f0dc0f4c3f918eba9b54e4529cd750e8
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-23 23:22:35 -0800

    Updates

---
+++ precache-manifest.128337b38f2a0df663b926e805485c62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51e32094448708600b5fe36254e2a57",
+    "url": "/SwapCentral-Sale/index.html"
+  },
+  {
+    "revision": "f6d46f9ca6e92301bd67",
+    "url": "/SwapCentral-Sale/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b9fcb90d575198ea42c",
+    "url": "/SwapCentral-Sale/static/js/2.735250a8.chunk.js"
+  },
+  {
+    "revision": "f6d46f9ca6e92301bd67",
+    "url": "/SwapCentral-Sale/static/js/main.5048a13d.chunk.js"
+  },
+  {
+    "revision": "b3ba0e105f27e672b5f6",
+    "url": "/SwapCentral-Sale/static/js/runtime-main.60ab17fd.js"
+  }
+]);
