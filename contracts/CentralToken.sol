d896ff95060a576a06129cc42bad97e8962ca7c5
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-26 13:32:53 -0800

    Updated with comments

--- contracts/CentralToken.sol
+++ contracts/CentralToken.sol
@@ -5,7 +5,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title CentralToken
 contract CentralToken is ERC20 {
+    /// @notice
+    /// @dev mints initial token supply to the deployer's account
+    /// @param initialSupply
     constructor(uint256 initialSupply) ERC20("CentralToken", "SCT") {
         _mint(msg.sender, initialSupply);
     }
