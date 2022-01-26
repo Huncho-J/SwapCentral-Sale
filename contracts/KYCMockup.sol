857185ad868da372e5b843ed1d776d78fc0aed07
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-26 14:00:52 -0800

    Updated KYCMockup.sol with comments

--- contracts/KYCMockup.sol
+++ contracts/KYCMockup.sol
@@ -1,17 +1,31 @@
 pragma solidity 0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title KYCMockup
+  /// @notice Acts as whitelisting mechanism for a crowd sale or ICO
 contract KYCMockup is Ownable{
-  mapping (address => bool) allowed;
 
+  mapping (address => bool) allowed;
+  /**
+  /// @notice takes an address as input to be whitelisted
+  /// @dev sets user whitelisted status to true. makes use of openzepplins "onlyOwner". onlyOwner is the account that deployed this contract.
+   /// @param _addr
+  */
   function setKycCompleted(address _addr) public onlyOwner{
     allowed[_addr] = true;
   }
 
+  /// @notice takes an address as input and resets it's whitelisted status to false
+  /// @dev sets user status to false in "Allowed" mapping
+  /// @param _addr Address of account to revoke access.
   function setKycRevoked(address _addr) public onlyOwner{
     allowed[_addr] = false;
   }
 
+  /// @notice checks KYC status for an address
+  /// @dev checks to see if KYC has been completed for a specified address
+  /// @param _addr Address of a users account.
+  /// @return true or false based on the user's status set in "Allowed" mapping
   function kycCompleted(address _addr) public view returns(bool){
     return allowed[_addr];
   }
