165fc71e423da796387f94e74069f9dfe3b1bd71
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-27 08:08:55 -0800

    Truffle config update

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+ const mnemonic = require("./mnemonic.json").mnemonic
 
 module.exports = {
   networks: {
@@ -7,6 +11,14 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+    provider: () => new HDWalletProvider(mnemonic, `https://speedy-nodes-nyc.moralis.io/b339a04a1ce96516cea5132d/eth/ropsten`),
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
