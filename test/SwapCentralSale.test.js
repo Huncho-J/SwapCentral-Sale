f1a61bb5e9628667321b5b61ab1744c8dbca708d
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-28 07:55:53 -0800

    Update SwapCentralSale.test.js

--- test/SwapCentralSale.test.js
+++ test/SwapCentralSale.test.js
@@ -1,6 +1,6 @@
 const CentralToken = artifacts.require("CentralToken");
 const SwapCentralTokenSale = artifacts.require("SwapCentralTokenSale");
-const KYCMockup = artifacts.require("KYCMockup.sol")
+const KYCMockup = artifacts.require("KYCMockup")
 
 const chai = require("./chaiSetup.js");
 const BN = web3.utils.BN;
@@ -18,10 +18,11 @@ contract("SwapCentralTokenSale Test", async(accounts)=>{
   let centralToken, kyc, tokenSale;
 
   before(async () => {
-    centralToken = await CentralToken.deployed(process.env.INITIAL_TOKENS_SUPPLY);
-    kyc = await KYCMockup.deployed()
-    tokenSale = await SwapCentralTokenSale.deployed(1,deployer,centralToken.address,kyc.address)
-  })
+    centralToken = await CentralToken.new(process.env.INITIAL_TOKENS_SUPPLY);
+    kyc = await KYCMockup.new();
+    tokenSale = await SwapCentralTokenSale.new(1,deployer,centralToken.address,kyc.address);
+    await centralToken.transfer(tokenSale.address,process.env.INITIAL_TOKENS_SUPPLY);
+  });
 
   describe('SwapCentral deployment',async () => {
     it('deploys successfully',async () => {
@@ -30,12 +31,21 @@ contract("SwapCentralTokenSale Test", async(accounts)=>{
       assert.notEqual(address, '')
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
-    })
+    });
 
      it('currrently holds all tokens sent from deployer', async() => {
        let balance = await centralToken.balanceOf(tokenSale.address);
        console.log(balance.toString())
-      assert.equal(balance.toString(),1000000);
+       assert.equal(balance.toString(),1000000);
     });
   });
+  
+  describe('SwapCentral Sale',async () => {
+    it('allows investor to purchase tokens',async () => {
+      await kyc.setKycCompleted(investor, {from:deployer})
+      await tokenSale.buyTokens(investor, {from:investor, value:web3.utils.toWei('1', 'wei')});
+      let investorBal = await centralToken.balanceOf(investor);
+      assert.equal(investorBal.toString(),1);
+    });
+});
 });
