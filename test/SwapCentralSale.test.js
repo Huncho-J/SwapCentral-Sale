37aaed7d7e40c96f5d15cb4ac66b7b5c5fe1dd43
Huncho-J <77946521+Huncho-J@users.noreply.github.com>
2022-01-27 20:28:13 -0800

    Update SwapCentralSale.test.js

--- test/SwapCentralSale.test.js
+++ test/SwapCentralSale.test.js
@@ -1,5 +1,6 @@
 const CentralToken = artifacts.require("CentralToken");
 const SwapCentralTokenSale = artifacts.require("SwapCentralTokenSale");
+const KYCMockup = artifacts.require("KYCMockup.sol")
 
 const chai = require("./chaiSetup.js");
 const BN = web3.utils.BN;
@@ -7,23 +8,34 @@ const expect = chai.expect;
 
 require("dotenv").config({path: "../.env"});
 
+
+  function toWei(n){
+    return web3.utils.toWei(n, 'ether');
+  }
+
 contract("SwapCentralTokenSale Test", async(accounts)=>{
+  const [deployer, admin,investor] = accounts;
+  let centralToken, kyc, tokenSale;
 
   before(async () => {
-    this.centralToken = await CentralToken.deployed(process.env.INITIAL_TOKENS_SUPPLY);
+    centralToken = await CentralToken.deployed(process.env.INITIAL_TOKENS_SUPPLY);
+    kyc = await KYCMockup.deployed()
+    tokenSale = await SwapCentralTokenSale.deployed(1,deployer,centralToken.address,kyc.address)
   })
 
- const [deployer, admin, recipient ] = accounts;
-  let swapCentralTokenSale, centralToken, totalSupply;
+  describe('SwapCentral deployment',async () => {
+    it('deploys successfully',async () => {
+      const address = await tokenSale.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
 
-
-
-    it('currrently holds all tokens sent from deployer', async() => {
-      expect(this.centralToken.balanceOf(deployer)).to.eventually.be.a.bignumber.equal(new BN(0));
+     it('currrently holds all tokens sent from deployer', async() => {
+       let balance = await centralToken.balanceOf(tokenSale.address);
+       console.log(balance.toString())
+      assert.equal(balance.toString(),1000000);
     });
-
-    // it('facilitates token transfers', async() => {
-    //
-    // });
-
+  });
 });
